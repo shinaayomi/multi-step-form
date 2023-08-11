469e9d52b7849aa49f1fdafcaa907d8f5de22dd6
shinaayomi <lateef718@gmail.com>
2023-08-11 14:30:35 +0100

    changes

--- src/components/AppNavBar.tsx
+++ src/components/AppNavBar.tsx
@@ -37,8 +37,13 @@ export default function AppNavBar({
   ];
 
   const handleStepper = (val: number) => {
+    if (monthPlan === null && yearPlan === null) {
+      return;
+    }
+    if (checkedList === null || checkedProduct?.length === 0) {
+      return;
+    }
     setStep(val);
-
     localStorage.setItem("navigation", JSON.stringify(val));
   };
 
